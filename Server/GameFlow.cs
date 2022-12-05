d19b95313aa7fbce032fb63b7f0af5df8347f7e4
azamie <ishii.kami2001@gmail.com>
2022-12-05 19:55:13 +0700

    Init game flow

---
+++ Server/GameFlow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class GameFlow
+    {
+        public int numberOfPlayers
+        {
+            get; set;
+        }
+        public int turn
+        {
+            get; set;
+        }
+
+        public bool isStart
+        {
+            get; set;   
+        }
+
+        public string currentQuestion;
+
+        public string currentAnswer;
+
+        public GameFlow()
+        {
+            numberOfPlayers = 0;
+            turn = 0;
+            isStart = false;
+        }
+    }
+}

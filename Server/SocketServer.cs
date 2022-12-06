39e308f3d471c642c5959b5130f4f43df698823d
Pham An <anhoangpham2001@gmail.com>
2022-12-06 19:40:38 +0700

    Handle limited skipping turn

--- Server/SocketServer.cs
+++ Server/SocketServer.cs
@@ -161,21 +161,30 @@ namespace Server
             {
                 // Update the game flow: next player, same question 
                 // Send the question to the next player
-                GameState.nextPlayer();
-
-                if (GameState.isWin())
+                if (!GameState.setOfSkippedPlayers.Contains(GameState.getCurrentPlayer()))
                 {
-                    foreach (string player in GameState.setOfPlayers)
+                    GameState.playerSkip(GameState.getCurrentPlayer());
+                    GameState.nextPlayer();
+                    if (GameState.isWin())
                     {
-                        byte[] data1 = Encoding.ASCII.GetBytes("winner~" + GameState.currentPlayer);
-                        GameState.SocketAndPlayerMapping[player].Send(data1);
+                        foreach (string player in GameState.setOfPlayers)
+                        {
+                            byte[] data1 = Encoding.ASCII.GetBytes("winner~" + GameState.currentPlayer);
+                            GameState.SocketAndPlayerMapping[player].Send(data1);
+                        }
+                    }
+                    else
+                    {
+                        byte[] data = Encoding.ASCII.GetBytes(GameState.getCurrentQuestion());
+                        GameState.SocketAndPlayerMapping[GameState.getCurrentPlayer()].Send(data);
                     }
                 }
-                else
-                {
-                    byte[] data = Encoding.ASCII.GetBytes(GameState.getCurrentQuestion());
-                    GameState.SocketAndPlayerMapping[GameState.getCurrentPlayer()].Send(data);
+                else {
+                    byte[] data5 = Encoding.ASCII.GetBytes("skip_invalid");
+                    GameState.SocketAndPlayerMapping[GameState.getCurrentPlayer()].Send(data5);
                 }
+
+                
             }
             else if (text.ToLower().StartsWith("my_answer~"))// User answer the question
             {
